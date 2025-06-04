1c9214e9472d5d88a8b2ed13f42c39c90d8f7f25
andrewteece <andrewteece@gmail.com>
2025-06-04 10:00:11 -0500

    fix next.config and code course.ts

---
+++ src/drizzle/schema/course.ts
@@ -0,0 +1,17 @@
+import { relations } from 'drizzle-orm';
+import { pgTable, text } from 'drizzle-orm/pg-core';
+import { createdAt, id, updatedAt } from '../schemaHelpers';
+
+export const CourseTable = pgTable('courses', {
+  id,
+  name: text().notNull(),
+  description: text().notNull(),
+  createdAt,
+  updatedAt,
+});
+
+export const CourseRelationships = relations(CourseTable, ({ many }) => ({
+  courseProducts: many(CourseProductTable),
+  userCourseAccesses: many(UserCourseAccessTable),
+  courseSections: many(CourseSectionTable),
+}));
