71b27177f0ddd51984e61ca9164c9ed8101f3885
andrewteece <andrewteece@gmail.com>
2025-06-05 07:50:40 -0500

    product schema

---
+++ src/drizzle/schema/product.ts
@@ -0,0 +1,22 @@
+import { relations } from 'drizzle-orm';
+import { pgTable, text, integer, pgEnum } from 'drizzle-orm/pg-core';
+import { createdAt, id, updatedAt } from '../schemaHelpers';
+
+export const productStatuses = ['public', 'private'] as const;
+export type ProductStatus = (typeof productStatuses)[number];
+export const productStatusEnum = pgEnum('product_status', productStatuses);
+
+export const ProductTable = pgTable('products', {
+  id,
+  name: text().notNull(),
+  description: text().notNull(),
+  imageUrl: text().notNull(),
+  priceInDollars: integer().notNull(),
+  status: productStatusEnum().notNull().default('private'),
+  createdAt,
+  updatedAt,
+});
+
+export const ProductRelationships = relations(ProductTable, ({ many }) => ({
+  courseProducts: many(CourseProductTable),
+}));
