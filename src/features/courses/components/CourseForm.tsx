e6238a8f6e044840fc873e4cfc63342873aac42f
andrewteece <andrewteece@gmail.com>
2025-06-08 13:13:45 -0500

    fix drizzle dep and start CourseForm

--- src/features/courses/components/CourseForm.tsx
+++ src/features/courses/components/CourseForm.tsx
@@ -1,7 +1,24 @@
 'use client';
 
 import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { courseSchema } from '../schemas/courses';
+import { z } from 'zod';
 
-export function CourseForm() {
-  const form = useForm();
+export function CourseForm({
+  course,
+}: {
+  course?: {
+    id: string;
+    name: string;
+    description: '';
+  };
+}) {
+  const form = useForm<z.infer<typeof courseSchema>>({
+    resolver: zodResolver(courseSchema),
+    defaultValues: course ?? {
+      name: '',
+      description: '',
+    },
+  });
 }
