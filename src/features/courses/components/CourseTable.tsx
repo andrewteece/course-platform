ba9d45caa2d60d7550520e0fdb28654e0fa06aca
andrewteece <andrewteece@gmail.com>
2025-06-09 17:38:00 -0500

    code formatters.tx and update CourseTable imports

--- src/features/courses/components/CourseTable.tsx
+++ src/features/courses/components/CourseTable.tsx
@@ -1,3 +1,16 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { formatPlural } from '@/lib/formatters';
+import { Trash2Icon } from 'lucide-react';
+
 export function CourseTable({
   courses,
 }: {
@@ -6,8 +19,61 @@ export function CourseTable({
     name: string;
     sectionsCount: number;
     lessonsCount: number;
-    studentCount: number;
+    studentsCount: number;
   }[];
 }) {
-  return <h1>Course Table</h1>;
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>
+            {formatPlural(courses.length, {
+              singular: 'course',
+              plural: 'courses',
+            })}
+          </TableHead>
+          <TableHead>Students</TableHead>
+          <TableHead>Actions</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {courses.map((course) => (
+          <TableRow key={course.id}>
+            <TableCell>
+              <div className='flex flex-col gap-1'>
+                <div className='font-semibold'>{course.name}</div>
+                <div className='text-muted-foreground'>
+                  {formatPlural(course.sectionsCount, {
+                    singular: 'section',
+                    plural: 'sections',
+                  })}{' '}
+                  •{' '}
+                  {formatPlural(course.lessonsCount, {
+                    singular: 'lesson',
+                    plural: 'lessons',
+                  })}
+                </div>
+              </div>
+            </TableCell>
+            <TableCell>{course.studentsCount}</TableCell>
+            <TableCell>
+              <div className='flex gap-2'>
+                <Button asChild>
+                  <Link href={`/admin/courses/${course.id}/edit`}>Edit</Link>
+                </Button>
+                <ActionButton
+                  variant='destructiveOutline'
+                  requireAreYouSure
+                  action={deleteCourse.bind(null, course.id)}
+                >
+                  <Trash2Icon />
+                  <span className='sr-only'>Delete</span>
+                </ActionButton>
+              </div>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 }
