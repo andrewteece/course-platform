fb18d27670a5d3a763fcc381b1077032193eb549
andrewteece <andrewteece@gmail.com>
2025-06-11 13:50:17 -0500

    installs Tabs and start imports

--- src/app/admin/courses/[courseId]/edit/page.tsx
+++ src/app/admin/courses/[courseId]/edit/page.tsx
@@ -1,6 +1,11 @@
 import { PageHeader } from '@/components/PageHeader';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { DialogTrigger } from '@/components/ui/dialog';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { db } from '@/drizzle/db';
+import { CourseSectionTable, CourseTable, LessonTable } from '@/drizzle/schema';
+import { CourseForm } from '@/features/courses/components/CourseForm';
 import { notFound } from 'next/navigation';
 
 export default async function EditCoursePage({
