aa6e421a835ebb177a0c61075835613c2d6c548c
andrewteece <andrewteece@gmail.com>
2025-06-11 13:45:36 -0500

    install shadCn card and dialog

---
+++ src/app/admin/courses/[courseId]/edit/page.tsx
@@ -0,0 +1,15 @@
+import { PageHeader } from '@/components/PageHeader';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { notFound } from 'next/navigation';
+
+export default async function EditCoursePage({
+  params,
+}: {
+  params: Promise<{ courseId: string }>;
+}) {
+  const { courseId } = await params;
+  const course = await getCourseGlobalTag(courseId);
+
+  if (course === null) return notFound();
+}
